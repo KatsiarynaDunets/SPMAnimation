3f15763625d2325987f4af8ce49efeb0ba773266
KatsiarynaDunets <kate.dunets@gmail.com>
2023-10-04 23:43:30 +0200

    project created

---
+++ SPMAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMAnimation
+//
+//  Created by Kate on 04/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
